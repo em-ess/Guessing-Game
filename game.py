1017ebb3e8eedaa92ad012303f012e9939fbce17
Michelle Song <michssong@gmail.com>
2022-06-06 20:05:49 -0700

    Initial commit - recoding old exercise

--- game.py
+++ game.py
@@ -7,8 +7,18 @@
 # choose random num 1-100
 # repeat:
 #   get guess
-#   if guess incorrect:
-#     give hint
-#     increase number of guesses
-#   else:
-#     congratulate player
+#       if guess incorrect:
+#           give hint
+#           increase number of guesses
+#       else:
+#           congratulate player
+
+def guessing_game():
+    greeting = "DO YOU WANT TO PLAY A GAME? *Imagine I am the SAW dude on the tricycle*"
+    print(greeting) #if i want to change the greeting 
+
+    yes_no = input("Press Y)es N)o or I) like turtles")
+    if yes_no in "Yy":  # i'll let them slide with a lowercase
+        print("")
+    elif yes_no in "Nn":
+
