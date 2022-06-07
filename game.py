92bfaa710f67b9513437b8c138c3292a76a32945
Michelle Song <michssong@gmail.com>
2022-06-06 21:38:45 -0700

    Greeting added. Eng game function added

--- game.py
+++ game.py
@@ -14,11 +14,36 @@
 #           congratulate player
 
 def guessing_game():
-    greeting = "DO YOU WANT TO PLAY A GAME? *Imagine I am the SAW dude on the tricycle*"
+
+    greeting = "DO YOU WANT TO PLAY A GAME? *Imagine I am the SAW dude on the little tricycle*"
     print(greeting) #if i want to change the greeting 
 
-    yes_no = input("Press Y)es N)o or I) like turtles")
-    if yes_no in "Yy":  # i'll let them slide with a lowercase
-        print("")
-    elif yes_no in "Nn":
+    player_name = input("How would you like me to address you sir/maam?\n>")
 
+    yes_no = input("Type Y)es N)o or I) like turtles\n>")
+    
+    if yes_no in "Yy":  # i'll let them slide with a lowercase
+        print("Just guess my number from 1-100 inclusive...\n" 
+              "I didn't say it was a fun game...")        
+
+    elif yes_no in "Nn":
+        print("")
+        end_game(player_name)
+
+    elif yes_no in "Ii":
+        print("Hey me too dude. Me too.\n"
+              "I'll save you the trouble - don't play this game...")
+
+        end_game(player_name)
+
+    else:
+        print("Listen here bud. You should have typed a Y or N or I." 
+              "But since you didn't I will force you to play the game.")
+        
+    
+    player_guess = input("Guess my number from 1 - 100 inclusive...\n>")
+    
+
+
+def end_game(player_name):
+    print(f"Bye {player_name}. Hope you have a good day! Or don't. It's your life :)")
